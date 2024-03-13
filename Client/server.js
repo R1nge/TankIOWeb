5f6e50a676a04a074204089ca5d7696fed94ae50
R1nge <r1ngegamedev@gmail.com>
2024-03-13 21:29:01 +0300

    JS to GO web socket connection

---
+++ Client/server.js
@@ -0,0 +1,7 @@
+﻿let socket = new WebSocket("ws://localhost:8080", "echo-protocol");
+
+socket.onopen = function(e) {
+    alert("[open] Connection established");
+    alert("Sending to server");
+    socket.send("My name is John");
+};
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TankIOWeb
+
+Websockets + GO - backend
+Vanilla js - frontend
+
+Get input from the browser in js
+Send it to GO using web socket
+Execute the input on the GO side
+Send updated info to the js
