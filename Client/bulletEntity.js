6b9083169b9f7522fea50b417adb6b118d36fea9
R1nge <r1ngegamedev@gmail.com>
2024-03-13 20:55:28 +0300

    Client move bullet in direction

--- Client/bulletEntity.js
+++ Client/bulletEntity.js
@@ -1,14 +1,20 @@
-﻿function createBullet(radius, x, y) {
-    this.radius = radius
-    this.x = x
-    this.y = y
-    return this
+﻿function createBullet(radius, x, y, color) {
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    return this;
 }
 
 function draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "red";
+    ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
+}
+
+function move(speed, directionX, directionY, deltaTime) {
+    this.x += speed * directionX * deltaTime;
+    this.y -= speed * directionY * deltaTime;
 }
--- Client/renderer.js
+++ Client/renderer.js
@@ -3,13 +3,22 @@ const ctx = canvas.getContext("2d");
 const frameRate = 60;
 const deltaTime = 1 / frameRate;
 
-const bulletEntity = createBullet(5, canvas.width / 2 , canvas.height / 2);
+
+const bulletSpeed = 5;
+const bulletEntity = createBullet(
+    5, 
+    canvas.width / 2 , 
+    canvas.height / 2,
+    "red"
+);
+
 bulletEntity.draw();
 
 
 function render(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     bulletEntity.draw();
+    bulletEntity.move(bulletSpeed, 1, 1, deltaTime);
 }
 
-setInterval(render, deltaTime)
+setInterval(render, deltaTime);
