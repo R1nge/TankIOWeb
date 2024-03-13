f92e1166889b45756d23c86a6c7ad24ed6253078
R1nge <r1ngegamedev@gmail.com>
2024-03-13 20:49:07 +0300

    Client render bullet

---
+++ Client/bulletEntity.js
@@ -0,0 +1,14 @@
+﻿function createBullet(radius, x, y) {
+    this.radius = radius
+    this.x = x
+    this.y = y
+    return this
+}
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
---
+++ Client/renderer.js
@@ -0,0 +1,15 @@
+﻿const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const frameRate = 60;
+const deltaTime = 1 / frameRate;
+
+const bulletEntity = createBullet(5, canvas.width / 2 , canvas.height / 2);
+bulletEntity.draw();
+
+
+function render(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    bulletEntity.draw();
+}
+
+setInterval(render, deltaTime)
